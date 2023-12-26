37be89233cf8819893bb5f3716760a821ce308d0
Aldrin Lim <aldrinlim81@gmail.com>
2023-12-27 00:34:14 +0800

    chore: Order Card Item

---
+++ src/screens/Product/ProductMenu/components/OrderItemCard.tsx
@@ -0,0 +1,36 @@
+type OrderItemCardProps = {
+  id: string
+  name: string
+  quantity: number
+  image?: string
+}
+
+import ImageLoader from 'components/ImageLoader'
+import MiddleTruncatedText from 'components/MiddleTruncatedText'
+import './OrderItemCard.styles.css'
+
+const OrderItemCard = (props: OrderItemCardProps) => {
+  const { name, quantity, image } = props
+
+  return (
+    <div className="OrderItem card card-compact w-[155px] cursor-pointer border border-gray-300 bg-base-100">
+      <figure className="h-[155px] w-[155px] bg-gray-300">
+        {/* Show image or PhotoIcon based on image load status */}
+        <ImageLoader src={image} iconClassName="w-24 text-gray-400" />
+      </figure>
+      <div className="card-body flex flex-col gap-0 !py-2 text-left">
+        <h2 className="card-title text-sm">
+          <MiddleTruncatedText text={name} maxLength={18} />
+        </h2>
+
+        <div className="flex flex-row gap-1  text-xs">
+          <span className="overflow-hidden truncate text-ellipsis">
+            {quantity} available
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default OrderItemCard
--- src/screens/Product/ProductMenu/index.tsx
+++ src/screens/Product/ProductMenu/index.tsx
@@ -13,6 +13,9 @@ import { AppPath } from 'routes/AppRoutes.types'
 import { useState } from 'react'
 import SlidingTransition from 'components/SlidingTransition'
 import ProductSelection from './screens/ProductSelection'
+import ImageLoader from 'components/ImageLoader'
+import MiddleTruncatedText from 'components/MiddleTruncatedText'
+import OrderItemCard from './components/OrderItemCard'
 
 enum ProductMenuScreen {
   None = 'none',
@@ -33,30 +36,19 @@ const ProductMenu = () => {
         <Toolbar
           items={[
             <div key={1} />,
-            <ToolbarTitle key={2} title="Menu" />,
+            <ToolbarTitle key={2} title="Product Menu" />,
             <div key={3} />,
           ]}
         />
         <div className="flex w-full flex-row flex-wrap gap-4">
-          <button className="btn btn-square  mt-1 flex h-[150px] w-[150px] flex-col border-2 border-dashed border-gray-300 ">
+          <button className="btn btn-square  mt-1 flex h-[213px] w-[150px] flex-col border-2 border-dashed border-gray-300 ">
             <PlusIcon className="w-8 text-success" />
             Add Product
           </button>
-          <button className="btn btn-square  mt-1 flex h-[150px] w-[150px] flex-col border-2 border-dashed border-gray-300 ">
-            <PlusIcon className="w-8 text-success" />
-            Add Product
-          </button>
-          <button className="btn btn-square  mt-1 flex h-[150px] w-[150px] flex-col border-2 border-dashed border-gray-300 ">
-            <PlusIcon className="w-8 text-success" />
-            Add Product
-          </button>
-          <button className="btn btn-square  mt-1 flex h-[150px] w-[150px] flex-col border-2 border-dashed border-gray-300 ">
-            <PlusIcon className="w-8 text-success" />
-            Add Product
-          </button>
+          <OrderItemCard id="" name="New Product" quantity={33} />
         </div>
         <div className="fixed bottom-10 left-0 right-0 flex justify-center ">
-          <button className="CartButton btn mx-10 flex w-full max-w-md flex-shrink flex-row justify-center gap-4 rounded-md bg-purple-400 p-4 text-white">
+          <button className="CartButton btn mx-4 flex w-full max-w-md flex-shrink flex-row justify-center gap-4 rounded-md bg-purple-400 p-4 text-white">
             <div className="mx-auto flex flex-row gap-4">
               <div data-testid className="flex flex-row gap-1">
                 <ShoppingCartIcon className="w-4" />
