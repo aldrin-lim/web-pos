bac098559112ab25b5cc1302ea019adf53df8b86
Aldrin Lim <aldrinlim81@gmail.com>
2024-01-02 02:20:46 +0800

    Update index.tsx

--- src/screens/Product/ProductMenu/screens/ProductCollection/index.tsx
+++ src/screens/Product/ProductMenu/screens/ProductCollection/index.tsx
@@ -1,8 +1,4 @@
-import {
-  PlusIcon,
-  ShoppingCartIcon,
-  ChevronRightIcon,
-} from '@heroicons/react/24/solid'
+import { PlusIcon } from '@heroicons/react/24/solid'
 import Toolbar from 'components/Layout/components/Toolbar'
 import ToolbarTitle from 'components/Layout/components/Toolbar/components/ToolbarTitle'
 import { useProductMenuContext } from '../../context/ProductMenuContext'
@@ -84,23 +80,6 @@ const ProductCollection = (props: ProductCollectionProps) => {
           ))}
         </div>
       )}
-
-      {/* {!isLoading && (
-        <div className="CartButton fixed bottom-10 left-0 right-0 flex justify-center ">
-          <button className="CartButton btn mx-4 flex w-full max-w-md flex-shrink flex-row justify-center gap-4 rounded-md bg-purple-400 p-4 text-white shadow-md">
-            <div className="mx-auto flex flex-row gap-4">
-              <div data-testid className="flex flex-row gap-1">
-                <ShoppingCartIcon className="w-4" />
-                <p>0</p>
-              </div>
-              <div className="flex flex-row gap-1">
-                <p>₱ 0</p>
-              </div>
-            </div>
-            <ChevronRightIcon className="w-5" />
-          </button>
-        </div>
-      )} */}
     </div>
   )
 }
