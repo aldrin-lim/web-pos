ca98be8bbc507fe641060dc4334b5733af14d0d2
Aldrin Lim <aldrinlim81@gmail.com>
2024-02-20 17:09:43 +0800

    Update index.tsx

--- src/screens/Catalog/components/ProductList/index.tsx
+++ src/screens/Catalog/components/ProductList/index.tsx
@@ -75,7 +75,7 @@ const ProductList = (props: ProductListProps) => {
           {onAddProductClick && (
             <button
               onClick={onAddProductClick}
-              className="btn btn-square mt-1 flex h-[213px] w-[155px] flex-col justify-self-center border-2 border-dashed border-gray-300"
+              className="btn btn-square mt-1 flex h-[208px] w-[153px] flex-col justify-self-center border-2 border-dashed border-gray-300"
             >
               <PlusIcon className="w-8 text-success" />
               Add Product
