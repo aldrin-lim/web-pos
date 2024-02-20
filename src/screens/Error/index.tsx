3193ea2f300cf9af2634b323f0a42959f09922df
Aldrin Lim <aldrinlim81@gmail.com>
2024-02-21 00:35:51 +0800

    Update index.tsx

--- src/screens/Error/index.tsx
+++ src/screens/Error/index.tsx
@@ -1,7 +1,12 @@
-import { FaceFrownIcon } from '@heroicons/react/24/outline'
+import { useAuth0 } from '@auth0/auth0-react'
+import {
+  ArrowLeftOnRectangleIcon,
+  FaceFrownIcon,
+} from '@heroicons/react/24/outline'
 import { ArrowPathIcon } from '@heroicons/react/24/solid'
 
 const Error = () => {
+  const { logout } = useAuth0()
   const refreshPage = () => {
     // Go back to the original url without the paths
     window.location.href = window.location.origin
@@ -27,10 +32,17 @@ const Error = () => {
         </a>
       </p>
 
-      <div className="mt-10">
+      <div className="mt-10 flex flex-col gap-4">
         <button className="btn flex flex-row gap-2 " onClick={refreshPage}>
           <ArrowPathIcon className="w-6" />
           Refresh page
+        </button>
+        <button
+          onClick={() => logout()}
+          className="btn flex w-full flex-row gap-2"
+        >
+          <ArrowLeftOnRectangleIcon className="w-6" />
+          Sign out
         </button>
       </div>
     </div>
