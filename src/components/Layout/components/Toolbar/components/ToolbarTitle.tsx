32cf91a294bf15023c10e7ebe688ee9ba5953bd1
qrafterClarence <clarence.castillo@qrafter.io>
2024-03-14 21:45:49 +0800

    Update h1 color
    Update h1 color

--- src/components/Layout/components/Toolbar/components/ToolbarTitle.tsx
+++ src/components/Layout/components/Toolbar/components/ToolbarTitle.tsx
@@ -5,7 +5,7 @@ type ToolbarTitleProps = {
 
 const ToolbarTitle: React.FC<ToolbarTitleProps> = ({ title, description }) => {
   return (
-    <div className="flex flex-col gap-0 self-center py-3 text-center">
+    <div className="flex flex-col gap-0 self-center py-3 text-center text-white">
       <h1 className="font-bold">{title}</h1>
       {description && <p className="text-xs">{description}</p>}
     </div>
