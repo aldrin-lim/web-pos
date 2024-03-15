5e36670f65c17ebc0b4b71a957e5b5d9c3b6df3e
qrafterClarence <clarence.castillo@qrafter.io>
2024-03-15 20:40:52 +0800

    Border focus update
    Border focus update

--- src/screens/Shift/StartShift/index.tsx
+++ src/screens/Shift/StartShift/index.tsx
@@ -104,7 +104,7 @@ const StartShift = () => {
               value={getFieldProps('openingPettyCash').value}
               type="text"
               tabIndex={3}
-              className="input input-bordered w-full"
+              className="input input-bordered w-full focus:border-teal-500"
               prefix={'₱'}
               onValueChange={(value) => {
                 setFieldValue('openingPettyCash', value)
@@ -130,7 +130,7 @@ const StartShift = () => {
             </div> */}
             <textarea
               {...getFieldProps('notes')}
-              className="textarea textarea-bordered"
+              className="textarea textarea-bordered focus:border-teal-500"
             />
 
             {errors.notes && (
